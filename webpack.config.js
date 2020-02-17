9747bf829772916176b2a64b2a0a68217e06ce1a
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-18 02:41:34 +0300

    fix: lint webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 
 const path = require('path');
+
 module.exports = {
   entry: {
-    app: './src/index.js',
+    main: './src/index.js',
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
 };
