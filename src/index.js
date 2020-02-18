545f18c4fd7fb2e45bfada6d33729aa2fce6b199
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-18 06:19:59 +0300

    feat: set event listners for click events - on add buttons

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+import renderProjectForm from './modules/projects/addProjectForm';
+import renderTaskForm from './modules/tasks/addTaskForm';
+
+// Clean container
+const clearContent = elementID => {
+  const div = document.getElementById(elementID);
+  div.removeChild(div.firstChild);
+};
+
+//  Listen for clickevents
+document.getElementById('btn-add-project').addEventListener('click', () => {
+  clearContent('add-form');
+  document.getElementById('add-form').appendChild(renderProjectForm());
+});
+
+document.getElementById('btn-add-task').addEventListener('click', () => {
+  
+  document.getElementById('add-form').appendChild(renderTaskForm());
+});
