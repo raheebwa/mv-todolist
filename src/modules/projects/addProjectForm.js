10479037c377f0de390e0c5e0789af0cb59a9e8b
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-18 04:39:08 +0300

    feat: Js generated project form

---
+++ src/modules/projects/addProjectForm.js
@@ -0,0 +1,27 @@
+// find form container
+const formContainer = document.getElementById('project-form');
+
+// create form
+const form = document.createElement('form');
+
+// create form fields
+const textBox = document.createElement('input');
+textBox.setAttribute('type', 'text');
+textBox.setAttribute('class', 'form-control my-2');
+textBox.setAttribute('placeholder', 'Project Name');
+textBox.setAttribute('id', 'project-name');
+
+const submitBtn = document.createElement('button');
+submitBtn.setAttribute('type', 'submit');
+submitBtn.setAttribute('class', 'btn btn-outline-dark btn-block');
+submitBtn.innerText = 'Create Project';
+
+const renderProjectForm = () => {
+  form.appendChild(textBox);
+  form.appendChild(submitBtn);
+  formContainer.appendChild(form);
+
+  return formContainer;
+};
+
+export default renderProjectForm;
