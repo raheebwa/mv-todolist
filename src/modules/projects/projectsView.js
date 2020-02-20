8eaf7aa60d6148409df7d63d2e38c9c5a593d603
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-21 02:57:18 +0300

    feat: can add projects

--- src/modules/projects/projectsView.js
+++ src/modules/projects/projectsView.js
@@ -1,21 +1,37 @@
 
+const renderListItem = (content) => {
+  const listItem = document.createElement('a');
+  listItem.setAttribute('class', 'border border-dark rounded-0 list-group-item list-group-item-action');
+  listItem.innerHTML = content;
+  return listItem
+}
+
 const projectsView = {
   all(list) {
     const listGroup = document.createElement('div');
     listGroup.setAttribute('class', 'list-group border border-dark');
+    listGroup.setAttribute('id', 'project-list-group');
 
     list.forEach(lItem => {
-      const listItem = document.createElement('a');
-      listItem.setAttribute('class', 'border border-dark rounded-0 list-group-item list-group-item-action');
-      listItem.innerHTML = lItem;
+      const listItem = renderListItem(lItem);
       listGroup.appendChild(listItem);
     });
     return listGroup;
   },
-  // todo
-  // find(project){}
-  // show(project)
-  // delete(project){}
+
+  add(){
+    const projValue = document.getElementById('projectName');
+
+    const listGroup = document.getElementById('project-list-group');
+
+    const form = document.getElementById('project-form');
+    form.addEventListener('submit', function (e) {
+      const listItem = renderListItem(projValue.value);
+      listGroup.appendChild(listItem);
+      e.preventDefault(); 
+    });
+
+  }
 };
 
 export default projectsView;
