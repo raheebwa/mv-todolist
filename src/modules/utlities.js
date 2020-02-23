a7c4f4cb779fbe94e34ecf8f594788bede9af655
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-24 01:52:13 +0300

    feat: add utitilies for across app usage

---
+++ src/modules/utlities.js
@@ -0,0 +1,23 @@
+const mySafeString = myS => {
+  myS = myS.toLowerCase().replace(' ', '-');
+  return myS;
+};
+
+// Clear Inner HTML
+const clearContent = elementID => {
+  const div = document.getElementById(elementID);
+  div.removeChild(div.firstChild);
+};
+
+//  Delete from array
+const deleteValue = (arr, index) => {
+  arr.splice(index, 1);
+  var filtered = arr.filter(function (el) {
+    return el != null;
+  });
+  return filtered;
+}
+
+
+
+export { mySafeString, clearContent, deleteValue };
