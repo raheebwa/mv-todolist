5170dbd4fe80451f49829ea575b808de12113636
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-24 03:42:19 +0300

    feat: refractor form to do both edit and add

--- src/modules/tasks/addTaskForm.js
+++ src/modules/tasks/addTaskForm.js
@@ -5,29 +5,51 @@ const form = document.createElement('form');
 form.setAttribute('id', 'task-form');
 form.className = ('d-none');
 // create form fields
-const textBox = document.createElement('input');
+const textBox = (value = null) => {
+  let textBox = document.createElement('input');
 textBox.setAttribute('type', 'text');
 textBox.setAttribute('class', 'form-control my-2');
 textBox.setAttribute('placeholder', 'Task title');
 textBox.setAttribute('id', 'taskName');
 textBox.setAttribute('name', 'task-name');
-
-const textArea = document.createElement('textarea');
+if (value != null) {
+  textBox.setAttribute('value',value);
+}
+  return textBox
+}; 
+const textArea = (value = null) => {
+  let textArea = document.createElement('textarea');
 textArea.setAttribute('class', 'form-control my-2');
 textArea.setAttribute('placeholder', 'Task Description');
 textArea.setAttribute('id', 'taskDesc');
 textArea.setAttribute('name', 'task-desc');
+  if (value != null) {
+    textArea.setAttribute('value', value);
+  }
+  return textArea
+}
 
-const textDate = document.createElement('input');
+const textDate = (value = null) =>{
+  let textDate = document.createElement('input');
 textDate.setAttribute('type', 'date');
 textDate.setAttribute('class', 'form-control my-2');
 textDate.setAttribute('id', 'due-date');
 textDate.setAttribute('name', 'due-date');
+if (value != null) {
+  textDate.setAttribute('value', value);
+}
+  return textDate;
+}
 
-const renderSelect = (id, options) => {
+
+const renderSelect = (id, options, value = null) => {
   const select = document.createElement('select');
   select.setAttribute('class', 'form-control my-2');
   select.setAttribute('id', id);
+
+  if (value != null) {
+    pop.setAttribute('selected', value);
+  }
 
   let count = 0;
   options.forEach(opt => {
@@ -35,28 +57,34 @@ const renderSelect = (id, options) => {
     option.setAttribute('value', count);
     option.innerText = opt;
     select.appendChild(option);
+    if (value != null && value == count) {
+      option.setAttribute('selected', 'selected');
+    }
     count += 1;
   });
 
   return select;
 };
 
-const selectPriority = renderSelect('select-priority', ['None', 'Low', 'Medium', 'High']);
-const selectProjects = renderSelect('select-projects', projectsModel.allProjects());
+const selectPriority = (value = null) => { 
+  return renderSelect('select-priority', ['None', 'Low', 'Medium', 'High'], (value != null)? value: null );
+}
+const selectProjects = (value = null) => { 
+  return renderSelect('select-projects', projectsModel.allProjects(), (value != null)? value: null );
+}
 
 const submitBtn = document.createElement('button');
 submitBtn.setAttribute('type', 'submit');
 submitBtn.setAttribute('class', 'btn btn-outline-dark btn-block my-4');
-submitBtn.innerText = 'Create Task';
+submitBtn.innerText = 'Submit Task';
 
-const renderTaskForm = () => {
-  form.appendChild(textBox);
-  form.appendChild(textArea);
-  form.appendChild(textDate);
-  form.appendChild(selectPriority);
-  form.appendChild(selectProjects);
+const renderTaskForm = (title, desc, date, priority, project) => {
+  form.appendChild(textBox(title));
+  form.appendChild(textArea(desc));
+  form.appendChild(textDate(date));
+  form.appendChild(selectPriority(priority));
+  form.appendChild(selectProjects(project));
   form.appendChild(submitBtn);
-
   return form;
 };
 
